9d5a928967bb975bb5fe141dd3b952cdc2bc1c75
devcamilabarros <devcamilabarros@gmail.com>
2023-03-28 14:35:38 -0300

    Create ex008.js

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 
+console.log(`A velocidade do seu carro é ${vel} Km/h.`) //apresentar/escrever no terminal do node.js
+if (vel > 60) {  //Condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!') 
